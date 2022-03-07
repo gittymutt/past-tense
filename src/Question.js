889ba68ec06cd2c79d67ca5825668404e2ba1506
gittymutt <pedroschmitz@hotmail.com>
2022-03-07 11:59:18 -0500

    add record component

--- src/Question.js
+++ src/Question.js
@@ -2,20 +2,26 @@ import React from "react"
 import AudioPlayer from 'react-h5-audio-player';
 import 'react-h5-audio-player/lib/styles.css';
 import play from "./sounds/play.m4a"
+import Recorder from "./Recorder"
+// import data from "./data.js"
+
 // https://developer.mozilla.org/en-US/docs/Web/API/MediaRecorder
 
 export default function Question() {
     function speak(word) {
-        console.log("filename: " + `./sounds/${word}.m4a`)
         let sound = new Audio(require(`./sounds/${word}.m4a`))
         sound.play()
     }
+    let data = [1]
+    let questionData = data.map((d) =>{
+        return (
+            <section className="question">
+                <button onClick={() => speak("play")} >play</button>
+                <button onClick={() => speak("played")}>played</button>
+                <Recorder />
+            </section>
+        )
+    })
 
-    return (
-        <>
-        <button onClick={() => speak("play")} >play</button>
-        <button onClick={() => speak("played")}>played</button>
-        
-  </>
-    )
+    return (<>{questionData}</>   )
 }
