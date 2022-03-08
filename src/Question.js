17c9a476e7d377bf71155160678f9c7f1953ab5c
gittymutt <pedroschmitz@hotmail.com>
2022-03-07 20:16:08 -0500

    add sound files and data

--- src/Question.js
+++ src/Question.js
@@ -7,22 +7,34 @@ import data from "./data.js"
 // https://developer.mozilla.org/en-US/docs/Web/API/MediaRecorder
 
 export default function Question() {
+    const [qNo, setQNo] = React.useState(0)
+
     function speak(word) {
         let sound = new Audio(require(`./sounds/${word}`))
         sound.play()
+    }
+
+    function nextQuestion() {
+        setQNo((oldValue) => oldValue+1)
     }
     
     let questionData = data.map((d) =>{
         console.log(d.baseForm)
         let id = nextId("q")
+        
         return (
+            <>
             <section key={id} className="question">
                 <button onClick={() => speak(d.baseFormSf)} >{d.baseForm}</button>
                 <button onClick={() => speak(d.pastFormSf)}>{d.pastForm}</button>
                 <Recorder />
             </section>
+            <button onClick={nextQuestion}>Next</button>
+            </>
         )
     })
-
-    return (<>{questionData}</>   )
+    let currentQuestion = questionData[qNo]
+    return (<>
+    <p>Question number {qNo}</p>
+    {currentQuestion}</>   )
 }
--- src/data.js
+++ src/data.js
@@ -1,9 +1,185 @@
 const data = [
     {
         baseForm: "play",
-        baseFormSf: "play.m4a",
+        baseFormSf: "play.mp3",
         pastForm: "played",
-        pastFormSf: "playedm4a"
+        pastFormSf: "played.mp3"
+     },
+
+     /*
+     {
+        baseForm: "stop",
+        baseFormSf: "stop.mp3",
+        pastForm: "stopped",
+        pastFormSf: "stopped.mp3"
+     },
+     {
+        baseForm: "finish",
+        baseFormSf: "finish.mp3",
+        pastForm: "finished",
+        pastFormSf: "finished.mp3"
+     },
+     {
+        baseForm: "laugh",
+        baseFormSf: "laugh.mp3",
+        pastForm: "laughed",
+        pastFormSf: "laughed.mp3"
+     },
+     {
+        baseForm: "look",
+        baseFormSf: "look.mp3",
+        pastForm: "looked",
+        pastFormSf: "looked.mp3"
+     },
+     {
+        baseForm: "miss",
+        baseFormSf: "miss.mp3",
+        pastForm: "missed",
+        pastFormSf: "missed.mp3"
+     },
+     {
+        baseForm: "stop",
+        baseFormSf: "stop.mp3",
+        pastForm: "stopped",
+        pastFormSf: "stopped.mp3"
+     },
+     {
+        baseForm: "watch",
+        baseFormSf: "watch.mp3",
+        pastForm: "watched",
+        pastFormSf: "watched.mp3"
+     },
+     {
+        baseForm: "work",
+        baseFormSf: "work.mp3",
+        pastForm: "worked",
+        pastFormSf: "worked.mp3"
+     },
+     {
+        baseForm: "talk",
+        baseFormSf: "talk.mp3",
+        pastForm: "talked",
+        pastFormSf: "talked.mp3"
+     }, 
+     {
+        baseForm: "kiss",
+        baseFormSf: "kiss.mp3",
+        pastForm: "kissed",
+        pastFormSf: "kissed.mp3"
+     }, 
+     {
+        baseForm: "wash",
+        baseFormSf: "wash.mp3",
+        pastForm: "washed",
+        pastFormSf: "washed.mp3"
+     },
+     {
+        baseForm: "start",
+        baseFormSf: "start.mp3",
+        pastForm: "started",
+        pastFormSf: "started.mp3"
+     },
+     {
+        baseForm: "want",
+        baseFormSf: "want.mp3",
+        pastForm: "wanted",
+        pastFormSf: "wanted.mp3"
+     },
+     {
+        baseForm: "invite",
+        baseFormSf: "invite.mp3",
+        pastForm: "invited",
+        pastFormSf: "invited.mp3"
+     },
+     {
+        baseForm: "download",
+        baseFormSf: "download.mp3",
+        pastForm: "downloaded",
+        pastFormSf: "downloaded.mp3"
+     },
+     {
+        baseForm: "sound",
+        baseFormSf: "sound.mp3",
+        pastForm: "sounded",
+        pastFormSf: "sounded.mp3"
+     },
+     {
+        baseForm: "delete",
+        baseFormSf: "delete.mp3",
+        pastForm: "deleted",
+        pastFormSf: "deleted.mp3"
+     },
+     {
+        baseForm: "wait",
+        baseFormSf: "wait.mp3",
+        pastForm: "waited",
+        pastFormSf: "waited.mp3"
+     },
+     {
+        baseForm: "decide",
+        baseFormSf: "decide.mp3",
+        pastForm: "decided",
+        pastFormSf: "decided.mp3"
+     },
+     {
+        baseForm: "enjoy",
+        baseFormSf: "enjoy.mp3",
+        pastForm: "enjoyed",
+        pastFormSf: "enjoyed.mp3"
+     },
+     {
+        baseForm: "enter",
+        baseFormSf: "enter.mp3",
+        pastForm: "entered",
+        pastFormSf: "entered.mp3"
+     },
+     {
+        baseForm: "show",
+        baseFormSf: "show.mp3",
+        pastForm: "showed",
+        pastFormSf: "showed.mp3"
+     },
+     {
+        baseForm: "study",
+        baseFormSf: "study.mp3",
+        pastForm: "studied",
+        pastFormSf: "studied.mp3"
+     },
+     {
+        baseForm: "listen",
+        baseFormSf: "listen.mp3",
+        pastForm: "listened",
+        pastFormSf: "listened.mp3"
+     },
+     {
+        baseForm: "change",
+        baseFormSf: "change.mp3",
+        pastForm: "changed",
+        pastFormSf: "changed.mp3"
+     },
+     {
+        baseForm: "engage",
+        baseFormSf: "engage.mp3",
+        pastForm: "engaged",
+        pastFormSf: "engaged.mp3"
+     },
+     {
+        baseForm: "live",
+        baseFormSf: "live.mp3",
+        pastForm: "lived",
+        pastFormSf: "lived.mp3"
+     },
+     
+     
+     */
+     
+     
+     
+     {
+        baseForm: "shake",
+        baseFormSf: "shake.mp3",
+        pastForm: "shook",
+        pastFormSf: "shook.mp3"
      }
 ]
 
