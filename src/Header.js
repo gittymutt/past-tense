bf41a426d57e5ff436b2caec29d928f0fbebcfed
gittymutt <pedroschmitz@hotmail.com>
2022-04-01 14:58:49 -0400

    Move dropdown list

--- src/Header.js
+++ src/Header.js
@@ -11,6 +11,14 @@ export default function Header(props) {
             <label className="regular-checkbox" htmlFor="regular">Regular Verbs</label>
             <input onChange={props.chooseVerbType} type="radio" id="irregular" name="isRegular" value="irregular" />
             <label htmlFor="irregular">Irregular Verbs</label>
+            <button>Search</button>
+            <ul className="letter-list">
+                <li>a</li>
+                <li>b</li>
+                <li>c</li>
+                <li>d</li>
+                <li>e</li>
+            </ul>
         </header>
     )
 }
